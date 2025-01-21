ab8111e0322977f5212fecf7c1b5f52ea04175c1
pradeep <114591246+pradeep-jatav@users.noreply.github.com>
2025-01-21 17:06:08 +0530

    Update index.tsx

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -174,7 +174,7 @@ const Task = ({ task }: TaskProps) => {
     >
       {task.attachments && task.attachments.length > 0 && (
         <Image
-          src={`https://pm-s3-images.s3.us-east-2.amazonaws.com/${task.attachments[0].fileURL}`}
+          src={`https://pm-s3-imag-es.s3.us-east-1.amazonaws.com/${task.attachments[0].fileURL}`}
           alt={task.attachments[0].fileName}
           width={400}
           height={200}
